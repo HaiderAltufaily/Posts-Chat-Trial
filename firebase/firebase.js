88bc8a6a301b1e40ebccee994acaf30de08fd458
Haider <haiderlek@gmail.com>
2021-10-06 19:08:07 +0300

    test

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,17 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 const firebaseConfigs = {
-  apiKey: process.env.key,
+  apiKey: process.env.NEXT_PUBLIC_KEY,
 
-  authDomain: process.env.authDomain,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
 
-  projectId: process.env.projectId,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
 
-  storageBucket: process.env.storageBucket,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
 
-  messagingSenderId: process.env.messagingSender,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER,
 
-  appId: process.env.appId,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 const app = initializeApp(firebaseConfigs);
 
